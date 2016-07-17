51897db71c6da4bfd762fcecdddfcaef9c681e19
hunter <hantek.f@gmail.com>
2016-07-18 00:12:56 +0200

    first commit

---
+++ app/models/app.rb
@@ -0,0 +1,15 @@
+class App < ActiveRecord::Base
+	belongs_to :user
+	belongs_to :category
+	belongs_to :review
+	has_many :reviews
+	acts_as_votable
+	has_attached_file :app_img, :styles => { :app_index => "250x350>", :app_show => "325x475>" }, :default_url => "/images/:style/missing.png"
+    validates_attachment_content_type :app_img, :content_type => /\Aimage\/.*\Z/
+    translates :description
+ 
+  def self.find_by(*args)
+    self.where(*args).take
+  end
+ 
+end
---
+++ config/routes.rb
@@ -0,0 +1,43 @@
+Rails.application.routes.draw do
+
+scope "(:locale)", locale: /en|de/ do
+  resources :apps
+end
+# config/routes.rb
+Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  root to: "apps#index"
+end
+# end translate 
+
+  devise_for :users
+  resources :categories
+  resources :apps do
+  resources :reviews
+  end
+  #root 'apps#index'
+
+ 
+	resources :apps do
+
+    collection do
+      get 'search'
+    end
+    resources :reviews, except: [:show, :index]
+  end
+  scope "(:locale)", locale: /en|de/ do
+  resources :categories
+  get 'store' => 'store#index'
+  get 'desc' => 'store#desc'
+  get 'blind' => 'store#blind'
+  get 'sight' => 'store#sight'
+  get 'motorical' => 'store#motorical'
+  get 'learning' => 'store#learning'
+  get 'hearing' => 'store#hearing'
+
+
+  match 'store/:id' => 'store#show', :as => :store_product, :via => :get
+
+end
+
+end
